a6a222d7ff1e17348347a1480eafa7f6014f7950
unknown <guruluckystacker@gmail.com>
2022-02-05 02:52:56 -0300

    a

--- client/src/layouts/main/MainFooter.js
+++ client/src/layouts/main/MainFooter.js
@@ -132,7 +132,7 @@ export default function MainFooter() {
             textAlign: { xs: 'center', md: 'left' }
           }}
         >
-          © 2022. All rights reserved
+          © 2022. All rights reserved   
         </Typography>
       </Container>
     </RootStyle>
